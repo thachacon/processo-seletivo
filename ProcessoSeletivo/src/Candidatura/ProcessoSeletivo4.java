a7b8988b87171c6d71a0b5f5e5e04f094dd85c85
thachacon <thachacon@gmail.com>
2024-07-17 18:21:46 -0300

    processo 4

---
+++ ProcessoSeletivo/src/Candidatura/ProcessoSeletivo4.java
@@ -0,0 +1,43 @@
+package Candidatura;
+
+import java.util.Random;
+
+public class ProcessoSeletivo4 {
+    public static void main(String[] args) {
+        
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO", "AUGUSTO"};
+
+        for (String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato (String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato +" NA " + tentativasRealizadas + " TENTATIVA");
+		else
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADAS");
+		
+		
+	}
+
+    //método auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3)==1;	
+	}
+}
